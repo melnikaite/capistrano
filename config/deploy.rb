7f43dd8ac52a97541680a92983840b429f174ffe
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2012-03-23 13:15:37 +0300

    using different environments

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,15 +4,20 @@ set :repository,  "git@github.com:melnikaite/capistrano.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "172.16.16.177"                          # Your HTTP server, Apache/etc
-role :app, "172.16.16.177"                          # This may be the same as your `Web` server
-role :db,  "172.16.16.177", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
 set :deploy_to, "/usr/share/nginx/www"
 set :user, "capistrano"
 set :scm_username, "melnikaite"
 default_run_options[:pty] = true
+set :branch, fetch(:branch, "master")
+set :env, fetch(:env, "current")
+
+task :current do
+  server "172.16.16.177", :app, :web, :db, :primary => true
+end
+
+task :production do
+  server "172.16.16.177", :app, :web, :db, :primary => true
+end
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
