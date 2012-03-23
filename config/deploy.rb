c78861d3e83dc6b30b587797559760aec9c51a9b
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2012-03-23 21:53:59 +0300

    deployment using rvm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,68 @@
 set :application, "capistrano"
-set :repository,  "git@github.com:melnikaite/capistrano.git"
+set :keep_releases, 3
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# RVM integration
+# http://beginrescueend.com/integration/capistrano/
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_ruby_string, "1.9.3-p125"
+set :rvm_type, :user
 
-set :deploy_to, "/usr/share/nginx/www"
+# Bundler integration (bundle install)
+# http://gembundler.com/deploying.html
+require "bundler/capistrano"
+set :bundle_without, [:development, :test]
+
 set :user, "capistrano"
 set :password, "capistrano"
-set :scm_username, "melnikaite"
+set :deploy_to, "/usr/share/nginx/www"
+set :use_sudo, false
+
+# Must be set for the password prompt from git to work
+# http://help.github.com/deploy-with-capistrano/
 default_run_options[:pty] = true
-set :branch, fetch(:branch, "master")
-set :env, fetch(:env, "current")
-set :keep_releases, 3
-after "deploy", "deploy:cleanup"
-after "deploy:update_code" do
-  run ["ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"].join(" && ")
-end
+set :scm, :git
+set :repository, "git@github.com:melnikaite/capistrano.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 
+# Multiple Stages Without Multistage Extension
+# https://github.com/capistrano/capistrano/wiki/2.x-Multiple-Stages-Without-Multistage-Extension
+desc "Deploy using internal address"
 task :current do
+  set :rails_env, "current"
   server "172.16.16.177", :app, :web, :db, :primary => true
 end
 
+desc "Deploy using external address"
 task :production do
+  set :rails_env, "production"
   server "172.16.16.177", :app, :web, :db, :primary => true
 end
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# http://modrails.com/documentation/Users%20guide%20Nginx.html#capistrano
+namespace :deploy do
+  desc "Start server"
+  task :start, :roles => :app do
+    run "#{try_sudo} touch #{File.join(release_path,'tmp','restart.txt')}"
+  end
+  
+  # not supported by Passenger server
+  task :stop, :roles => :app do
+  end
+  
+  desc "Restart server"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(release_path,'tmp','restart.txt')}"
+  end
+  
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    #run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+  end
+end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+after 'deploy:update_code', 'deploy:symlink_shared'
+after "deploy", "deploy:migrate", "deploy:cleanup"
+
